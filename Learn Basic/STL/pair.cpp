d5b09a2c9ee8ed9d75f2b9f0b5f312f1a199df31
Ankit Kumar Biswal <116702754+AnkitkumarBiswal04@users.noreply.github.com>
2024-06-09 14:05:59 +0530

    Create pair.cpp

---
+++ Learn Basic/STL/pair.cpp
@@ -0,0 +1,24 @@
+//this has all the libraries
+#include<bits/stdc++.h>
+//this helps in acessing the library
+using namespace std;
+//this is the main function
+int main(){
+    //taking input for n
+    int n;
+    cin>>n;
+    //taking input for m
+    int m;
+    cin>>m;
+    //using pair 
+    pair<int,int>p={2,3};
+    //to take user input 
+    // we can also write pair<int,int>p={n,m};
+    //printing the pair 
+    cout<<p.first<<" "<<p.second;
+    cout<<endl;
+        pair<int,int> arr[]={{1,2},{3,4},{5,6}};
+        cout<<arr[0].first<<" "<<arr[0].second;
+        
+    
+}
